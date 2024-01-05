f3f3c13689bf183f9a2f2437a01ed1f806b197c7
Nanda Kishore Maddineni <115626959+nandaKishore536@users.noreply.github.com>
2024-01-05 21:48:09 +0530

    Update index.js

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,71 +1,35 @@
-.cart-summary-container {
-  display: flex;
-  flex-direction: column;
-  align-self: flex-end;
-}
+import CartContext from '../../context/CartContext'
 
-.order-total-value {
-  color: #171f46;
-  font-family: 'Roboto';
-  font-size: 16px;
-  margin-bottom: 2px;
-}
+import './index.css'
 
-@media screen and (min-width: 768px) {
-  .order-total-value {
-    font-size: 32px;
-  }
-}
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      let total = 0
+      cartList.forEach(eachCartItem => {
+        total += eachCartItem.price * eachCartItem.quantity
+      })
 
-.order-total-label {
-  color: #616e7c;
-  font-family: 'Roboto';
-  font-size: 16px;
-  font-weight: 500;
-}
+      return (
+        <>
+          <div className="cart-summary-container">
+            <h1 className="order-total-value">
+              <span className="order-total-label">Order Total:</span> Rs {total}
+              /-
+            </h1>
+            <p className="total-items">{cartList.length} Items in cart</p>
+            <button type="button" className="checkout-button d-sm-none">
+              Checkout
+            </button>
+          </div>
+          <button type="button" className="checkout-button d-lg-none">
+            Checkout
+          </button>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
 
-@media screen and (min-width: 768px) {
-  .order-total-label {
-    font-size: 24px;
-  }
-}
-
-.total-items {
-  color: #616e7c;
-  font-family: 'Roboto';
-  font-size: 12px;
-  margin-top: 2px;
-}
-
-@media screen and (min-width: 768px) {
-  .total-items {
-    font-size: 16px;
-  }
-}
-
-.checkout-button {
-  background-color: #0b69ff;
-  color: #ffffff;
-  font-family: 'Roboto';
-  font-size: 12px;
-  border: none;
-  border-radius: 4px;
-  padding-top: 12px;
-  padding-bottom: 12px;
-  margin-top: 8px;
-  margin-bottom: 8px;
-  outline: none;
-  cursor: pointer;
-}
-
-@media screen and (max-width: 767px) {
-  .d-sm-none {
-    display: none;
-  }
-}
-
-@media screen and (min-width: 768px) {
-  .d-lg-none {
-    display: none;
-  }
-}
+export default CartSummary
